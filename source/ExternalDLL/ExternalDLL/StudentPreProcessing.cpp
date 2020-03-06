e3f57f93d4209cb8e92da0d4e5e1f45637c2f6c5
Wietse <WietseTDam@gmail.com>
2020-03-06 17:50:38 +0100

    Updated student preprocessing

--- source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
+++ source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
@@ -1,4 +1,7 @@
 #include "StudentPreProcessing.h"
+#include "ImageIO.h"
+#include "ImageFactory.h"
+#include "HereBeDragons.h"
 
 
 IntensityImage * StudentPreProcessing::stepToIntensityImage(const RGBImage &image) const {
@@ -10,7 +13,39 @@ IntensityImage * StudentPreProcessing::stepScaleImage(const IntensityImage &imag
 }
 
 IntensityImage * StudentPreProcessing::stepEdgeDetection(const IntensityImage &image) const {
-	return nullptr;
+	cv::Mat input_matrix;
+	HereBeDragons::HerLoveForWhoseDearLoveIRiseAndFall(image, input_matrix); // ImageToMatrix
+	//cv::medianBlur(*image, *image, 3);
+	//cv::GaussianBlur(*image, *image, cv::Size(3, 3), 0, 0, cv::BORDER_DEFAULT);
+	cv::Mat kernel = (cv::Mat_<float>(9, 9) << 0, 0, 0, 1, 1, 1, 0, 0, 0,
+		0, 0, 0, 1, 1, 1, 0, 0, 0,
+		0, 0, 0, 1, 1, 1, 0, 0, 0,
+		1, 1, 1, -4, -4, -4, 1, 1, 1,
+		1, 1, 1, -4, -4, -4, 1, 1, 1,
+		1, 1, 1, -4, -4, -4, 1, 1, 1,
+		0, 0, 0, 1, 1, 1, 0, 0, 0,
+		0, 0, 0, 1, 1, 1, 0, 0, 0,
+		0, 0, 0, 1, 1, 1, 0, 0, 0);
+	cv::Mat kernel_x = (cv::Mat_<float>(3, 3) << 1, 0, -1,
+												 5, 0, -5,
+												 1, 0, -1);
+	cv::Mat kernel_y = (cv::Mat_<float>(3, 3) << 1, 5, 1,
+												 0, 0, 0,
+												 -1, -5, -1);
+
+	cv::Mat output_matrix_x;
+	cv::Mat output_matrix_y;
+	filter2D(input_matrix, output_matrix_x, CV_8U, kernel_x, cv::Point(-1, -1), 0, cv::BORDER_DEFAULT);
+	filter2D(input_matrix, output_matrix_y, CV_8U, kernel_y, cv::Point(-1, -1), 0, cv::BORDER_DEFAULT);
+	cv::Mat output_matrix = cv::Mat_<float>(output_matrix_x.cols, output_matrix_x.rows);
+	for (int i = 0; i < output_matrix_x.rows; i++) {
+		for (int j = 0; j < output_matrix_x.cols; j++) {
+			output_matrix.at<float>(i, j, 0) = output_matrix_x.at<float>(i, j, 0); //+ output_matrix_y.at<float>(j, i, 0)) / 2;
+		}
+	}
+	IntensityImage* new_image_output = ImageFactory::newIntensityImage();
+	HereBeDragons::NoWantOfConscienceHoldItThatICall(output_matrix, *new_image_output);  // MatrixToImage
+	return new_image_output;
 }
 
 IntensityImage * StudentPreProcessing::stepThresholding(const IntensityImage &image) const {
--- source/ExternalDLL/ExternalDLL/StudentPreProcessing.h
+++ source/ExternalDLL/ExternalDLL/StudentPreProcessing.h
@@ -3,7 +3,6 @@
 * Unauthorized copying of this file, via any medium is strictly prohibited
 * Proprietary and confidential
 */
-
 #pragma once
 #include "PreProcessing.h"
 class StudentPreProcessing : public PreProcessing {
